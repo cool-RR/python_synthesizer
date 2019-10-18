5e968ae3e9a083100d2f511edebe786a7cf510f1
Ram Rachum <ram@rachum.com>
2019-10-18 21:57:08 +0300

    Play a sequence of notes

--- synthesizer.py
+++ synthesizer.py
@@ -10,12 +10,7 @@ HALF_LIFE = 0.3
 
 overtones = {i: 1 / (i ** 1.5) for i in range(1, 8)}
 
-class Note:
-    def __init__(self, frequency, volume=1):
-        self.frequency = frequency
-        self.volume = volume
-        self.length = 1.5
-
+class Audio:
     def play(self, samplerate=8000):
         time_array = np.linspace(0, self.length,
                                  int(self.length * samplerate))
@@ -24,6 +19,12 @@ class Note:
             pressure_array[i] = self.get_pressure(t.item())
         sounddevice.play(pressure_array, samplerate=samplerate)
         sounddevice.wait()
+
+class Note(Audio):
+    def __init__(self, frequency, volume=1):
+        self.frequency = frequency
+        self.volume = volume
+        self.length = 1.5
 
     def get_pressure(self, t):
         if 0 <= t <= self.length:
@@ -37,7 +38,23 @@ class Note:
         else:
             return 0
 
+class Sequence(Audio):
+    def __init__(self, offsets_and_notes):
+        self.offsets_and_notes = tuple(offsets_and_notes)
+        self.length = max(offset + note.length for offset, note
+                          in self.offsets_and_notes)
+
+    def get_pressure(self, t):
+        return sum(note.get_pressure(t - offset) for offset, note in
+                   self.offsets_and_notes)
+
 
 if __name__ == '__main__':
-    note = Note(440)
-    note.play()
+    sequence = Sequence((
+        (0.0, Note(440)),
+        (0.2, Note(440)),
+        (0.4, Note(440)),
+        (0.6, Note(440)),
+        (0.8, Note(440)),
+    ))
+    sequence.play()
