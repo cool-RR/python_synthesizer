4ec5c329ed7a8b112753a33cc167e49bfd146f37
Ram Rachum <ram@rachum.com>
2019-10-18 17:45:39 +0300

    Make overtones have weaker volumes the higher they get

--- synthesizer.py
+++ synthesizer.py
@@ -11,12 +11,15 @@ FREQUENCY = 440
 MASTER_VOLUME = 0.02
 HALF_LIFE = 0.3
 
+overtones = {i: 1 / (i ** 1.5) for i in range(1, 8)}
 
 def sine(t):
     volume = MASTER_VOLUME * 2 ** (-t / HALF_LIFE)
     result = 0
-    for i in range(1, 8):
-        result += math.sin(math.tau * t * FREQUENCY * i)
+    for overtone, overtone_volume in overtones.items():
+        result += overtone_volume * math.sin(
+            math.tau * t * FREQUENCY * overtone
+        )
     return volume * result
 
 
