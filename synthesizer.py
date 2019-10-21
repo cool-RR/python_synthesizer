ecf9bb2e5f1e8326c1a42b7b87300f82cee0c96a
Ram Rachum <ram@rachum.com>
2019-10-21 15:01:17 +0300

    Add Note class

--- synthesizer.py
+++ synthesizer.py
@@ -5,32 +5,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sounddevice
 
-DURATION = 2
-SAMPLERATE = 8000
-FREQUENCY = 440
+
 MASTER_VOLUME = 0.2
-HALF_LIFE = 0.3
 
-overtones = {i: 1 / (i ** 1.5) for i in range(1, 8)}
+class Note:
+    overtones = {i: 1 / (i ** 1.5) for i in range(1, 8)}
+    half_life = 0.3
 
-def sine(t):
-    volume = MASTER_VOLUME * 2 ** (-t / HALF_LIFE)
-    result = 0
-    for overtone, overtone_volume in overtones.items():
-        result += overtone_volume * math.sin(
-            math.tau * t * FREQUENCY * overtone
-        )
-    return volume * result
+    def __init__(self, frequency, volume=1):
+        self.frequency = frequency
+        self.volume = volume
+        self.length = 1.5
+
+    def play(self, samplerate=8000):
+        time_array = np.linspace(0, self.length,
+                                 int(self.length * samplerate))
+        pressure_array = np.zeros(time_array.shape, dtype='d')
+        for i, t in enumerate(time_array):
+            pressure_array[i] = self.get_pressure(t.item())
+        sounddevice.play(pressure_array, samplerate=samplerate,
+                         blocking=True)
+
+    def get_pressure(self, t):
+        if (0 <= t <= self.length):
+            result = 0
+            for overtone, overtone_volume in self.overtones.items():
+                result += overtone_volume * math.sin(
+                    math.tau * t * self.frequency * overtone
+                )
+            volume = (MASTER_VOLUME * self.volume *
+                      2 ** (-t / self.half_life))
+            result *= volume
+            return result
+        else:
+            return 0
 
 
-time_array = np.linspace(0, DURATION, DURATION * SAMPLERATE)
-
-pressure_array = np.zeros(time_array.shape, dtype='d')
-for i, t in enumerate(time_array):
-    pressure_array[i] = sine(t.item())
-
-plt.plot(pressure_array[:60])
-plt.show()
-
-sounddevice.play(pressure_array, samplerate=SAMPLERATE, blocking=True)
-
+if __name__ == '__main__':
+    note = Note(440)
+    note.play()
