2145efd640240e4c02c1d7be79f633ce80354730
Ram Rachum <ram@rachum.com>
2019-10-21 15:01:17 +0300

    Add Note class

--- synthesizer.py
+++ synthesizer.py
@@ -5,33 +5,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sounddevice
 
-DURATION = 2
-SAMPLERATE = 8000
-FREQUENCY = 440
 MASTER_VOLUME = 0.02
 HALF_LIFE = 0.3
 
 overtones = {i: 1 / (i ** 1.5) for i in range(1, 8)}
 
-def sine(t):
-    volume = MASTER_VOLUME * 2 ** (-t / HALF_LIFE)
-    result = 0
-    for overtone, overtone_volume in overtones.items():
-        result += overtone_volume * math.sin(
-            math.tau * t * FREQUENCY * overtone
-        )
-    return volume * result
+class Note:
+    def __init__(self, frequency, volume=1):
+        self.frequency = frequency
+        self.volume = volume
+        self.length = 1.5
+
+    def play(self, samplerate=8000):
+        time_array = np.arange(0, self.length, 1 / samplerate)
+        pressure_array = np.zeros_like(time_array)
+        for i, t in enumerate(time_array):
+            pressure_array[i] = self.get_pressure(t.item())
+        sounddevice.play(pressure_array, samplerate=samplerate)
+        sounddevice.wait()
+
+    def get_pressure(self, t):
+        if 0 <= t <= self.length:
+            result = 0
+            for overtone, overtone_volume in overtones.items():
+                result += overtone_volume * math.sin(
+                    math.tau * t * self.frequency * overtone
+                )
+            volume = MASTER_VOLUME * self.volume * 2 ** (-t / HALF_LIFE)
+            return result * volume
+        else:
+            return 0
 
 
-time_array = np.arange(0, DURATION, 1 / SAMPLERATE)
-
-pressure_array = np.zeros_like(time_array)
-for i, t in enumerate(time_array):
-    pressure_array[i] = sine(t.item())
-
-plt.plot(pressure_array[:60])
-plt.show()
-
-sounddevice.play(pressure_array, samplerate=SAMPLERATE)
-sounddevice.wait()
-
+if __name__ == '__main__':
+    note = Note(440)
+    note.play()
