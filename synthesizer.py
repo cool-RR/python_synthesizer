369a1670362142d62834f320c04734b08cc777a8
Ram Rachum <ram@rachum.com>
2020-07-07 17:17:09 +0300

    Plot and play a sine wave

---
+++ synthesizer.py
@@ -0,0 +1,28 @@
+#!python3
+import math
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sounddevice
+
+DURATION = 2
+SAMPLERATE = 8000
+FREQUENCY = 440
+MASTER_VOLUME = 0.04
+
+def sine(t):
+    return MASTER_VOLUME * math.sin(math.tau * t * FREQUENCY)
+
+
+time_array = np.arange(0, DURATION, 1 / SAMPLERATE)
+
+pressure_array = np.zeros_like(time_array)
+for i, t in enumerate(time_array):
+    pressure_array[i] = sine(t.item())
+
+plt.plot(pressure_array[:60])
+plt.show()
+
+sounddevice.play(pressure_array, samplerate=SAMPLERATE)
+sounddevice.wait()
+
